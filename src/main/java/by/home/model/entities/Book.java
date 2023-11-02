50965f3462f6eb53d5099abc451809f32a38568b
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-11-02 15:07:42 +0300

    extended class with fields and annotation

--- src/main/java/by/home/model/entities/Book.java
+++ src/main/java/by/home/model/entities/Book.java
@@ -1,4 +1,19 @@
 package by.home.model.entities;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+
+    private int pages;
+
+
 }
