92505ee5d1fce04c23a66340cff361d44154457f
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-11-02 15:57:18 +0300

    added ManyToOne annotation

--- src/main/java/by/home/model/entities/Book.java
+++ src/main/java/by/home/model/entities/Book.java
@@ -1,19 +1,28 @@
 package by.home.model.entities;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@Data
 @Entity
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NonNull
     private String title;
 
+    @NonNull
     private int pages;
+    @ManyToOne
+    private Library library;
 
 
 }
