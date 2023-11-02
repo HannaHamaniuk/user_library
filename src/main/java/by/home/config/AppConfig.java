e8f88a49ed30b1c972d584b0b21fde1dffb376db
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-11-02 13:04:36 +0300

    added viewResolver method

--- src/main/java/by/home/config/AppConfig.java
+++ src/main/java/by/home/config/AppConfig.java
@@ -1,4 +1,21 @@
 package by.home.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan("by.home")
 public class AppConfig {
+
+    @Bean
+    private ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages");
+        resolver.setSuffix(".jsp");
+        return  resolver;
+    }
+
 }
